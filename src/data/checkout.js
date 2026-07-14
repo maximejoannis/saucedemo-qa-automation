922b3ceb8312412426964259efb73060f9a39227
maximejoannis <maxime.joannis6@gmail.com>
2026-07-14 14:04:54 +0200

    Add files via upload

--- src/data/checkout.js
+++ src/data/checkout.js
@@ -1,1 +1,3 @@
-
+module.exports = {
+  validCustomer: { firstName: 'Maxime', lastName: 'JOANNIS', postalCode: '94340' },
+};
---
+++ src/data/products.js
@@ -0,0 +1,19 @@
+const products = {
+  backpack: {
+    slug: 'sauce-labs-backpack',
+    name: 'Sauce Labs Backpack',
+    price: 29.99,
+  },
+  bikeLight: {
+    slug: 'sauce-labs-bike-light',
+    name: 'Sauce Labs Bike Light',
+    price: 9.99,
+  },
+  boltTShirt: {
+    slug: 'sauce-labs-bolt-t-shirt',
+    name: 'Sauce Labs Bolt T-Shirt',
+    price: 15.99,
+  },
+};
+
+module.exports = { products };
