621075075d5702b6f321d0d70466f88085207246
maximejoannis <maxime.joannis6@gmail.com>
2026-07-14 14:09:32 +0200

    Add files via upload

--- src/fixtures/test.js
+++ src/fixtures/test.js
@@ -1,1 +1,30 @@
+const base = require('@playwright/test');
+const { LoginPage } = require('../pages/LoginPage');
+const { InventoryPage } = require('../pages/InventoryPage');
+const { CartPage } = require('../pages/CartPage');
+const { CheckoutPage } = require('../pages/CheckoutPage');
+const { standard } = require('../data/users');
 
+const test = base.test.extend({
+  loginPage: async ({ page }, use) => {
+    await use(new LoginPage(page));
+  },
+  inventoryPage: async ({ page }, use) => {
+    await use(new InventoryPage(page));
+  },
+  cartPage: async ({ page }, use) => {
+    await use(new CartPage(page));
+  },
+  checkoutPage: async ({ page }, use) => {
+    await use(new CheckoutPage(page));
+  },
+  authenticatedPage: async ({ page, loginPage, inventoryPage }, use) => {
+    await loginPage.goto();
+    await loginPage.login(standard.username, standard.password);
+    await base.expect(page).toHaveURL(/\/inventory\.html$/);
+    await base.expect(inventoryPage.title).toBeVisible();
+    await use(page);
+  },
+});
+
+module.exports = { test, expect: base.expect };
