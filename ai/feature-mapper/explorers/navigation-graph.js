f0afb1a9ea85b23bdf9c1001c16101c3b1dab4b6
maximejoannis <maxime.joannis6@gmail.com>
2026-08-01 23:19:39 +0200

    Create navigation-graph.js

---
+++ ai/feature-mapper/explorers/navigation-graph.js
@@ -0,0 +1,62 @@
+class NavigationGraph {
+  constructor() {
+    this.pages = new Map();
+    this.transitions = [];
+  }
+
+  addPage(observation) {
+    if (!observation || !observation.url) {
+      return;
+    }
+
+    if (!this.pages.has(observation.url)) {
+      this.pages.set(observation.url, {
+        name: observation.title,
+        url: observation.url,
+        buttons: observation.buttons || [],
+        links: observation.links || [],
+        inputs: observation.inputs || [],
+      });
+    }
+  }
+
+  addTransition(from, action, to) {
+    if (!from || !to) {
+      return;
+    }
+
+    const exists = this.transitions.some(
+      (transition) =>
+        transition.from === from &&
+        transition.to === to &&
+        transition.action === action,
+    );
+
+    if (!exists) {
+      this.transitions.push({
+        from,
+        action,
+        to,
+      });
+    }
+  }
+
+  getPages() {
+    return [...this.pages.values()];
+  }
+
+  getTransitions() {
+    return [...this.transitions];
+  }
+
+  toJSON() {
+    return {
+      pages: this.getPages(),
+      transitions: this.getTransitions(),
+    };
+  }
+}
+
+module.exports = {
+  NavigationGraph,
+};
